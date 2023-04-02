f51a9a2b0b8ae0135a86771f0d7ecca91db7b924
Mike B <115735557+mbartnett@users.noreply.github.com>
2023-04-01 22:12:09 -0500

    quitting for the night, a lot of progress with the styles, not too much left

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,17 +1,19 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
 
 export default function Resume() {
     return (
         <section class="resume">
             <div class="resume-download">
                 <h2>RESUME</h2>
-                <p class="download">Download a copy</p>
+                <div class="download">Looking for a creative, detail-oriented developer and designer to collaborate with? Let's make it  happen.<br></br> <a href="https://docs.google.com/document/d/1UgkqdhqG1hgHg3m1zNvVetWkceQp1rI_QsaXWgP-cPs/edit" target="_blank" rel="noopener noreferrer">Download my resume</a> to see if I'm the right fit for you.<br></br>#opentowork</div>
             </div>
             <div class="resume-skills">
                 <h2>Skills</h2>
                 <p class="skills">HTML5 • CSS3 • UI/UX • JavaScript<br></br>Node.js • OOP • Express.js • APIs • MySQL<br></br>ORM • MVC • NoSQL • MongoDB • React<br></br>Redux • GraphQL • Webpack • jQuery<br></br>Wordpress • MailChimp • Creative Cloud
                 </p>
             </div>
-        </section>
+        </section >
     );
 }
+
